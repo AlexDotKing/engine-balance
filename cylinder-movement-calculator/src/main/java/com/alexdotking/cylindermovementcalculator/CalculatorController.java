e7697170cf618b6ff99047bf0adfc1f47b023ccd
jinyize <jinyize@wuxicloud.com>
2019-05-07 16:10:19 +0800

    初始提交

---
+++ cylinder-movement-calculator/src/main/java/com/alexdotking/cylindermovementcalculator/CalculatorController.java
@@ -0,0 +1,23 @@
+package com.alexdotking.cylindermovementcalculator;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class CalculatorController {
+
+    @Value("${defaultBore:1 default}")
+    private String defaultBore;
+
+    @Value("${defaultStroke:1 default}")
+    private String defaultStroke;
+
+    @RequestMapping(value = "/getDefaultBoreAndStroke", method = RequestMethod.GET)
+    public String getDefaultBoreAndStroke() {
+        return "defaultBore:" + defaultBore + "\ndefaultStroke:" + defaultStroke;
+    }
+}
---
+++ cylinder-movement-calculator/src/main/java/com/alexdotking/cylindermovementcalculator/CylinderMovementCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.alexdotking.cylindermovementcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CylinderMovementCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CylinderMovementCalculatorApplication.class, args);
+    }
+
+}
